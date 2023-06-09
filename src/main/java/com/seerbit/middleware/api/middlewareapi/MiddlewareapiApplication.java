7934cfd9e8d7235ea4592dca78a5dcea16dc324f
Ekene.Amah-A <Ekene.Amah-A@nlng.net>
2023-06-09 21:41:39 +0100

    initial to dev

---
+++ src/main/java/com/seerbit/middleware/api/middlewareapi/MiddlewareapiApplication.java
@@ -0,0 +1,13 @@
+package com.seerbit.middleware.api.middlewareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiddlewareapiApplication.class, args);
+	}
+
+}
